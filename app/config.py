719b493501038e70b772ea6cabc1b9b3d4e683ca
kkartikeya22 <120785187+kkartikeya22@users.noreply.github.com>
2025-03-24 14:16:42 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-BASE_URL = "http://localhost:8000"
+BASE_URL = "https://giva-software-url-shortner-updated.onrender.com/"
